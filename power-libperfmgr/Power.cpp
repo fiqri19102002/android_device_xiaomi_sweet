6f264cf1fb75256832507880cfe2a7dee4af5bb8
Arian <arian.kulmer@web.de>
2021-07-18 01:54:53 +0200

    sweet: power-libperfmgr: implement double tap to wake
    Change-Id: I73f95e32daf9e2c702662ac2c2c8182e6b8eb214
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- power-libperfmgr/Power.cpp
+++ power-libperfmgr/Power.cpp
@@ -28,8 +28,18 @@
 
 #include <mutex>
 
+#include <sys/ioctl.h>
+
 #include "PowerHintSession.h"
 #include "PowerSessionManager.h"
+
+#define SET_CUR_VALUE 0
+#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
+
+#define TOUCH_MAGIC 0x5400
+#define TOUCH_IOC_SETMODE TOUCH_MAGIC + SET_CUR_VALUE
+
+#define Touch_Doubletap_Mode 14
 
 namespace aidl {
 namespace google {
@@ -86,12 +96,16 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
             }
             mSustainedPerfModeOn = true;
             break;
+        case Mode::DOUBLE_TAP_TO_WAKE: {
+            int fd = open(TOUCH_DEV_PATH, O_RDWR);
+            int arg[2] = {Touch_Doubletap_Mode, enabled ? 1 : 0};
+            ioctl(fd, TOUCH_IOC_SETMODE, &arg);
+            break;
+        }
         case Mode::LAUNCH:
             if (mSustainedPerfModeOn) {
                 break;
             }
-            [[fallthrough]];
-        case Mode::DOUBLE_TAP_TO_WAKE:
             [[fallthrough]];
         case Mode::FIXED_PERFORMANCE:
             [[fallthrough]];
