4adeacde48a25dda675ab949892651635ee6dec4
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-06-03 08:22:31 +0700

    sweet: Migrate to prebuilt pixel power HAL
    * Import from hardware/xiaomi on lineage-20 branch
    * Remove namespace guards, custom hooks and dt2w features
    
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- device.mk
+++ device.mk
@@ -397,8 +397,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
-    hardware/google/pixel \
-    hardware/xiaomi
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
