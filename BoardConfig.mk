e20f4891f7746552c60232f1e46d7e7f7d2e8203
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-06-03 11:39:01 +0700

    sweet: hidl: Rename framework compatibility matrix file
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,7 +73,7 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
