d5e9bf2d8351badb2a75f5b74e5f2ab449cb6fb8
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-10-11 16:17:18 +0700

    sweet: init: Don't set marketname for USB
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- init/init_sweet.cpp
+++ init/init_sweet.cpp
@@ -124,7 +124,6 @@ void vendor_load_properties() {
     set_ro_product_prop("device", device);
     set_ro_product_prop("model", model);
     property_override("bluetooth.device.default_name", marketname.c_str());
-    property_override("vendor.usb.product_string", marketname.c_str());   
     property_override("ro.build.description", description.c_str());
     if (mod_device != "") {
         property_override("ro.product.mod_device", mod_device.c_str());
