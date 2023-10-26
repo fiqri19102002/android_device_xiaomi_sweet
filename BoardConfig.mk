93519d00125e0e08497c2e357670087141d1997a
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-10-26 15:44:35 +0700

    sweet: Drop Lineage livedisplay HAL
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,14 +173,6 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
-# Soong
-SOONG_CONFIG_NAMESPACES += xiaomiSm6150Vars
-SOONG_CONFIG_xiaomiSm6150Vars += \
-    livedisplay_support_anti_flicker \
-    livedisplay_support_sunlight_enhancement
-SOONG_CONFIG_xiaomiSm6150Vars_livedisplay_support_anti_flicker ?= false
-SOONG_CONFIG_xiaomiSm6150Vars_livedisplay_support_sunlight_enhancement ?= true
-
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2023-11-01
 
